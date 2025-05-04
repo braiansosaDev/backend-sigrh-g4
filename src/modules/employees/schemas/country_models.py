2b0bf5285b15602e175500be492b6f92cb3bda42
Franco Barraza <barrazafranco15@gmail.com>
2025-05-04 19:43:13 -0300

    Se modela en ORM country

---
+++ src/modules/employees/schemas/country_models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class CountryResponse(BaseModel):
+    id: int
+    name: str
+
+class CreateCountry(BaseModel):
+    name: str = Field(max_length=100)
+
+class UpdateCountry(BaseModel):
+    id: int
+    name: Optional[str] = None
