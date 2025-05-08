4882ee0fe8dc2da05265baac98d25b10d15150fc
Franco Barraza <barrazafranco15@gmail.com>
2025-05-07 21:20:43 -0300

    Creacion de user_id automatico

--- src/modules/employees/services/utils.py
+++ src/modules/employees/services/utils.py
@@ -1,9 +1,11 @@
 from typing import List
+from fastapi import HTTPException, status
 from sqlmodel import select
 from src.database.core import DatabaseSession
 from src.modules.employees.models.documents import Document
 from src.modules.employees.models.employee import Employee
 from src.modules.employees.models.work_history import WorkHistory
+from src.modules.employees.schemas.employee_models import CreateEmployee
 
 def get_single_work_history_by_id(
     db: DatabaseSession, employee_id: int, work_history_id: int
@@ -53,3 +55,17 @@ def get_employee_by_user_id(db: DatabaseSession, user_id: str) -> Employee:
 
 def get_all_employees(db: DatabaseSession):
     return db.exec(select(Employee)).all()
+
+def create_user_id(db: DatabaseSession, employee_request: CreateEmployee) -> str:
+    first_char = employee_request.first_name[0].lower()
+    last_name = employee_request.last_name.lower()
+    dni = employee_request.dni[-3:]
+    new_user_id = f"{first_char}{last_name}{dni}"
+
+    if db.exec(select(Employee).where(Employee.user_id == new_user_id)).one_or_none():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User ID already exists.",
+        )
+
+    return new_user_id
