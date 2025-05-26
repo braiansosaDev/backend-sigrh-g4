d75ec4c654434553cd3819d1020df69f734b5878
nachosag <ignacioborlenghi@gmail.com>
2025-05-26 23:34:56 +0000

    style: format code for improved readability and consistency in face recognition controller

--- src/modules/face_recognition/controllers/face_recognition_controller.py
+++ src/modules/face_recognition/controllers/face_recognition_controller.py
@@ -5,15 +5,27 @@ from src.database.core import DatabaseSession
 from fastapi import status
 import logging
 
-from src.modules.face_recognition.schemas.face_recognition_models import CreateFaceRegistration, FaceRecognitionBaseModel, UpdateFaceRegistration, VerifyFaceRegistration, OperationStatus
+from src.modules.face_recognition.schemas.face_recognition_models import (
+    CreateFaceRegistration,
+    FaceRecognitionBaseModel,
+    UpdateFaceRegistration,
+    VerifyFaceRegistration,
+    OperationStatus,
+)
 from src.modules.face_recognition.services import face_recognition_service
 
 
 logger = logging.getLogger("uvicorn.error")
-face_recognition_router = APIRouter(prefix="/face_recognition", tags=["face_recognition"])
+face_recognition_router = APIRouter(
+    prefix="/face_recognition", tags=["Face Recognition"]
+)
 
 
-@face_recognition_router.post("/register", response_model=FaceRecognitionBaseModel, status_code=status.HTTP_201_CREATED)
+@face_recognition_router.post(
+    "/register",
+    response_model=FaceRecognitionBaseModel,
+    status_code=status.HTTP_201_CREATED,
+)
 async def register_face(
     db: DatabaseSession,
     face_recognition: CreateFaceRegistration,
@@ -24,7 +36,9 @@ async def register_face(
     return face_recognition_service.create_face_register(db, face_recognition)
 
 
-@face_recognition_router.post("/", response_model=OperationStatus, status_code=status.HTTP_200_OK)
+@face_recognition_router.post(
+    "/", response_model=OperationStatus, status_code=status.HTTP_200_OK
+)
 async def verify_face(
     db: DatabaseSession,
     face_recognition: VerifyFaceRegistration,
@@ -36,7 +50,9 @@ async def verify_face(
     return face_recognition_service.verify_face(db, face_recognition)
 
 
-@face_recognition_router.patch("/update", response_model=FaceRecognitionBaseModel, status_code=status.HTTP_200_OK)
+@face_recognition_router.patch(
+    "/update", response_model=FaceRecognitionBaseModel, status_code=status.HTTP_200_OK
+)
 async def update_face(
     db: DatabaseSession,
     face_recognition: UpdateFaceRegistration,
@@ -48,7 +64,11 @@ async def update_face(
     return face_recognition_service.update_face_register(db, face_recognition)
 
 
-@face_recognition_router.post("/{event_type}", response_model=OperationStatus, status_code=status.HTTP_202_ACCEPTED)
+@face_recognition_router.post(
+    "/{event_type}",
+    response_model=OperationStatus,
+    status_code=status.HTTP_202_ACCEPTED,
+)
 async def register_attendance(
     event_type: str,
     db: DatabaseSession,
@@ -61,9 +81,17 @@ async def register_attendance(
 
     # Validar el tipo recibido (para evitar errores de tipeo)
     if event_type not in {"in", "out"}:
-        raise HTTPException(status_code=400, detail="Tipo de evento inválido. Usar 'in' o 'out'.")
+        raise HTTPException(
+            status_code=400, detail="Tipo de evento inválido. Usar 'in' o 'out'."
+        )
 
     # Asignar device según el tipo
-    device_id = "Totem de reconocimiento facial ingreso." if event_type == "in" else "Totem de reconocimiento facial egreso."
+    device_id = (
+        "Totem de reconocimiento facial ingreso."
+        if event_type == "in"
+        else "Totem de reconocimiento facial egreso."
+    )
 
-    return face_recognition_service.register_attendance(db, face_recognition, event_type=event_type, device_id=device_id)
+    return face_recognition_service.register_attendance(
+        db, face_recognition, event_type=event_type, device_id=device_id
+    )
