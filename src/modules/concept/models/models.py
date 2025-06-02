19e5dbbee0fc4f76bea317f2e647f818b016ab6a
Franco Barraza <barrazafranco15@gmail.com>
2025-06-02 18:36:07 -0300

    Update models.py

--- src/modules/concept/models/models.py
+++ src/modules/concept/models/models.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 class Concept(SQLModel, table=True):
     __tablename__ = "concept"  # type: ignore
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     # exportation_id: Optional[int]
     description: str = Field(default="")
     is_deletable: bool = Field(default=False)
