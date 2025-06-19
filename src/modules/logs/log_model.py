88d78e02ade286b7be9d079fe74ff26414e9fbc9
nachosag <ignacioborlenghi@gmail.com>
2025-06-19 00:30:15 -0300

    Add logger to patch_hour and patch_leave

--- src/modules/logs/log_model.py
+++ src/modules/logs/log_model.py
@@ -2,17 +2,18 @@ from datetime import datetime
 from enum import StrEnum
 from sqlmodel import Field, SQLModel
 
+
 class EntityType(StrEnum):
-  LICENCIA = "LICENCIA"
-  NOMINA = "NOMINA"
+    LICENCIA = "LICENCIA"
+    NOMINA = "NOMINA"
+
 
 class Log(SQLModel, table=True):
-  __tablename__: str = "logs" #type: ignore
+    __tablename__: str = "logs"  # type: ignore
 
-  id: int = Field(primary_key=True, index=True)
-  entity_id: int # id de la entidad
-  user_id: int # user que hizo el cambio
-  entity: EntityType
-  date_change: datetime = Field(default_factory=datetime.now)
-  description: str = Field(default="")
-  
+    id: int = Field(primary_key=True, index=True)
+    entity_id: int
+    user_id: int
+    entity: EntityType
+    date_change: datetime = Field(default_factory=datetime.now)
+    description: str = Field(default="")
